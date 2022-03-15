aff5720bf1fc5e8dbe7df16e7e0036434b24f4fc
Joelgashaw <eyoelgashaw3@gmail.com>
2022-03-15 11:18:37 -0700

    alx

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char ch = 'a';
+	while(ch <= 'z')
+	{
+	printf("%c", ch);
+	ch++
+	}
+	return (0);
+}
+	
