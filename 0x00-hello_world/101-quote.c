209b2302554292ee3f21d5611c81f6acbf552c50
Joelgashaw <eyoelgashaw3@gmail.com>
2022-03-15 02:19:05 -0700

    git

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 /*
  * main - entry point
- *
  * Return: Always 0 (success)
  */
 
 int main(void)
 {
 	char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
 	while (*s) putchar (*s++);
+
 	return (0);
 }
