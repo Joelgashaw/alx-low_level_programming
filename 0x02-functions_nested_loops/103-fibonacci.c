6dca54c8aa0f2119529f2b53e8763fab718eb5f4
Joelgashaw <eyoelgashaw3@gmail.com>
2022-03-16 16:17:37 -0700

    fibonacci

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -9,14 +9,14 @@ int main(void)
 long i = 0;
 long j = 1;
 int n = 0;
-long fib;	
+long fib;
 while (n < 50)
 {
 fib = j + i;
 if (n != 49)
 printf("%ld, ", fib);
 else
-printf ("%ld\n", fib);
+printf("%ld\n", fib);
 i = j;
 j = fib;
 n += 1;
