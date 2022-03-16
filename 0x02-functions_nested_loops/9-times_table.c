a65528fd441431288ab3a3e2dd9cc8d0397a505d
Joelgashaw <eyoelgashaw3@gmail.com>
2022-03-16 12:31:02 -0700

    sa

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,12 +6,13 @@
 void times_table(void)
 {
 	int i, j, k;
+
 	for (i = 0; i <= 9; i++)
 	{
-		for(j = 0; j <= 9; j++)
+		for (j = 0; j <= 9; j++)
 		{
 			k = i * j;
-			if (j ==0)
+			if (j == 0)
 			{
 				_putchar(k + '0');
 			}
