6a22471115a16f64278c17e708c58c3a9a25d0e8
Joelgashaw <eyoelgashaw3@gmail.com>
2022-03-16 16:01:59 -0700

    ad

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,47 +1,25 @@
-#include <iostream.h>
-using namespace std;
+#include <stdio.h>
+/**
+ * main - prints the first 50 Fibonacci numbers starting with 1 and 2
+ * Return: int.
+ */
 
-int sum(int n);
-int fib(int);
-
-const int N = 4000000;
-
-int main() 
+int main(void)
 {
-	int i = 2;
-
-	cout << sum(i);
-
-	return 0;
-}
-
-int sum(int n) 
-{
-	int sum = 0; 
-
-	while (fib(n) < N) {
-
-		if (fib(n)%2==0) 
-		{
-			sum += fib(n);
-		}
-		n++;
-	}
-	return sum;
-}
-
-int fib(int n) {
-	int a = 0;
-	int b = 1;
-	int c = 0;
-	int i = 1;
-while (i <= n) {
-	c = a+ b;
-	a = b;
-	b - c;
-
-	i++;
-}
-return c;
-}
-
+	long i = 0;
+	long j = 1;
+	int n = 0;
+	long fib;
+	while (n < 50
+	{
+	fib = j + i;
+	if (n != 49)
+	 printf("%ld, ", fib);
+	 else
+	 printf ("%ld\n", fib);
+	 i = j;
+	 j = fib;
+	 n += 1;
+	 }
+	 return (0);
+	 }
