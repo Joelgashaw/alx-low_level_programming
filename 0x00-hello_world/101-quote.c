6ce7cfbec218fee2d5deac69686b47f34aa2257c
Joelgashaw <eyoelgashaw3@gmail.com>
2022-03-18 13:25:11 -0700

    more than

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,16 +2,13 @@
 #include <unistd.h>
 /*
  * main - entry point
- * print with out using printf and puts
+ *
  * Return: Always 0 (success)
  */
 
 int main(void)
 {
-	char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-	while (*s) putchar (*s++);
-
-	return (0);
-
+write(2, "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n",
+sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
+return (1);
 }
