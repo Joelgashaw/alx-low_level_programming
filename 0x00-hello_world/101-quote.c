ac67e32375a9076d714e939b286187810d6219ce
Joelgashaw <eyoelgashaw3@gmail.com>
2022-03-18 13:39:55 -0700

    ssd

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
 /*
- * main - entry point
+ * main - Entry point
  *
- * Return: Always 0 (success).
+ * Return: Always 0 (success)
  */
-
 int main(void)
 {
-write(2, "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n",
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
 sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
 return (1);
 }
