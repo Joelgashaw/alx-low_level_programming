c7bf93402ada9624a2bfc55312d5afb1f759d92d
Joelgashaw <eyoelgashaw3@gmail.com>
2022-03-25 13:25:37 -0700

    mal

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - program that prints all possible different combinations of 
+ * main - program that prints all possible different combinations of
  * three digits
  * Numbers must be separeted by ,followed by a space
  * The two digits must be different
@@ -17,7 +17,7 @@ int main(void)
 
 	while (n1 <= 57)
 	{
-		n2 = n1 +1;
+		n2 = n1 + 1;
 		while (n2 <= 57)
 		{
 			n3 = n2 + 1;
