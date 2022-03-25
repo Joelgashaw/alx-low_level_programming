c41b3a1933f64f2f5449b301bb747f8ed26fa4b5
Joelgashaw <eyoelgashaw3@gmail.com>
2022-03-25 12:29:47 -0700

    sd

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -2,7 +2,7 @@
 
 /**
  * main - program that prints all possible combination of songle digit
- * 
+ *
  * Numbers must be separeted by ,followed by a space
  * Numbes should be printed in ascending order
  * You can only use the putchar function
